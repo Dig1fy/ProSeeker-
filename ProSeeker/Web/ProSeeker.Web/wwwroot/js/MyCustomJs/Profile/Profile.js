468cd7870fc46bd6590aabec4f2023caa7e29331
Q2kPetrov <ivaylozp@gmail.com>
2020-11-30 01:24:44 +0200

    Adjust profile rendering.

--- ProSeeker/Web/ProSeeker.Web/wwwroot/js/MyCustomJs/Profile/Profile.js
+++ ProSeeker/Web/ProSeeker.Web/wwwroot/js/MyCustomJs/Profile/Profile.js
@@ -2,6 +2,6 @@
 function AddMoreServices() {
     
     $("#ServicesContainer").
-        append(" <div class='form-group col-md-10'><input type='text' class='form-control col-md-6 col' placeholder='Име на услугата' name='services[" + serviceIndex + "].Name' /> <br /> <textarea type='text' style='margin-top:-1rem' class='form-control col-md-12' placeholder='Описание на услугата' name='services[" + serviceIndex + "].Description'></textarea> </div>");
+        append(" <div class='form-group col-md-12'><input type='text' class='form-control col-md-10 col' placeholder='Име на услугата' name='services[" + serviceIndex + "].Name' /> <br /> <textarea type='text' style='margin-top:-1rem' class='form-control col-md-10' placeholder='Описание на услугата' name='services[" + serviceIndex + "].Description'></textarea> </div>");
     serviceIndex++;
 }
--- ProSeeker/Web/ProSeeker.Web/wwwroot/js/site.js
+++ ProSeeker/Web/ProSeeker.Web/wwwroot/js/site.js
@@ -1,8 +1,1 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-function getConfirmation() {
-    var retVal = confirm("Сигурни ли сте, че искате да изтриете това съдържание?");
-    return retVal === true;
-}
+﻿
