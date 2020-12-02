76b55c5f3ac64c90414cb559e3150d22182363c9
Q2kPetrov <ivaylozp@gmail.com>
2020-12-03 01:50:50 +0200

    Playing around with css/html. TODO: add reply text area to each opinion.

--- ProSeeker/Web/ProSeeker.Web/wwwroot/js/MyCustomJs/site.js
+++ ProSeeker/Web/ProSeeker.Web/wwwroot/js/MyCustomJs/site.js
@@ -1,13 +1,13 @@
-﻿function showOpinionsSection(parentId) {
-    // Show / hide opinions
-    let section = document.querySelector("#addOpinionsForm");
-    section.style.display = section.style.display === 'block' ? 'none' : 'block';
+﻿//function showOpinionsSection(parentId) {
+//    // Show / hide opinions
+//    let section = document.querySelector("#addOpinionsForm");
+//    section.style.display = section.style.display === 'block' ? 'none' : 'block';
 
-    // Insert parentId value according to the opinion(comment) we want to reply
-    $("#addOpinionsForm input[name='ParentId']").val(parentId);
+//    // Insert parentId value according to the opinion(comment) we want to reply
+//    $("#addOpinionsForm input[name='ParentId']").val(parentId);
 
-    // Navigate to the text area
-    $([document.documentElement, document.body]).animate({
-        scrollTop: $("#addOpinionsForm").offset().top
-    }, 1000);
-}
+//    // Navigate to the text area
+//    $([document.documentElement, document.body]).animate({
+//        scrollTop: $("#addOpinionsForm").offset().top
+//    }, 1000);
+//}
