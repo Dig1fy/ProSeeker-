fe33cfa836f8ccaf34f7bc476dee5ff132880acc
Q2kPetrov <ivaylozp@gmail.com>
2020-12-07 18:13:56 +0200

    Add quiz local images.

---
+++ ProSeeker/Web/ProSeeker.Web/Controllers/Quiz/QuizController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProSeeker.Web.Controllers.Quiz
+{
+    public class QuizController : BaseController
+    {
+        public IActionResult Quiz()
+        {
+            return this.View();
+        }
+    }
+}
