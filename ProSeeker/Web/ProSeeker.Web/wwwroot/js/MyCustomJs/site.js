75ef6700d80e2e2919db5fcdb1e3d37cc82a4902
Q2kPetrov <ivaylozp@gmail.com>
2020-12-08 16:38:21 +0200

    Small fixes (specialist's opinions.count/rating counter). Make all static files "copy always"

--- ProSeeker/Web/ProSeeker.Web/wwwroot/js/MyCustomJs/site.js
+++ ProSeeker/Web/ProSeeker.Web/wwwroot/js/MyCustomJs/site.js
@@ -1,15 +1,15 @@
-﻿function getRatings() {
-    let ratings = document.querySelectorAll('.number-rating');
-    let starsRef = document.querySelectorAll('.stars-inner');
-    const starsTotal = 5;
+﻿//function getRatings() {
+//    let ratings = document.querySelectorAll('.number-rating');
+//    let starsRef = document.querySelectorAll('.stars-inner');
+//    const starsTotal = 5;
 
-    for (i = 0; i < ratings.length; i++) {
-        let rating = ratings[i].textContent;
-        // Get percentage
-        const starPercentage = (rating / starsTotal) * 100;
-        // Round to nearest 10
-        const starPercentageRounded = `${Math.round(starPercentage / 10) * 10}%`;
-        // Set width of stars-inner to percentage            
-        starsRef[i].style.width = starPercentageRounded;
-    }
-}
+//    for (i = 0; i < ratings.length; i++) {
+//        let rating = ratings[i].textContent;
+//        // Get percentage
+//        const starPercentage = (rating / starsTotal) * 100;
+//        // Round to nearest 10
+//        const starPercentageRounded = `${Math.round(starPercentage / 10) * 10}%`;
+//        // Set width of stars-inner to percentage            
+//        starsRef[i].style.width = starPercentageRounded;
+//    }
+//}
