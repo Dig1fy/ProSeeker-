6e2542e664164f8a140ad92e864c1d60304b38e6
Q2kPetrov <ivaylozp@gmail.com>
2020-12-20 14:54:03 +0200

    Move sidebar css/js. Adjust live chat (displaying messages to sender/ local date time for each message)

--- ProSeeker/Web/ProSeeker.Web/Hubs/PrivateChatHub.cs
+++ ProSeeker/Web/ProSeeker.Web/Hubs/PrivateChatHub.cs
@@ -1,10 +1,11 @@
 ﻿namespace ProSeeker.Web.Hubs
 {
+    using System;
     using System.Threading.Tasks;
+
     using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.SignalR;
     using ProSeeker.Data.Models;
-    using ProSeeker.Data.Models.PrivateChat;
     using ProSeeker.Services.Data.PrivateChat;
 
     public class PrivateChatHub : Hub
@@ -26,15 +27,9 @@
         {
             var senderUser = await this.userManager.FindByIdAsync(senderId);
             var messageViewModel = await this.privateChatService.SendMessageToUserAsync(message, receiverId, senderId, conversationId);
-
-            //await this.Clients.User(messageViewModel.ReceiverId).SendAsync("SendMessage", messageViewModel);
-            await this.Clients.All.SendAsync("SendMessage", messageViewModel);
+            var adjustedDateTime = messageViewModel.CreatedOn;
+            messageViewModel.CreatedOn = DateTime.Parse(adjustedDateTime.ToString("O"));
+            await this.Clients.Users(messageViewModel.ReceiverId).SendAsync("SendMessage", messageViewModel);
         }
-
-        //public async Task Receive(string message, string receiverId, string senderId, string conversationId)
-        //{
-        //    await this.Clients.User(receiverId).SendAsync("SendMessage", message);
-        //    //await this.privateChatService.ReceiveNewMessage(message, receiverId, senderId, conversationId);
-        //}
     }
 }
---
+++ ProSeeker/Web/ProSeeker.Web/wwwroot/js/MyCustomJs/indexSidebar.js
@@ -0,0 +1,22 @@
+﻿function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.querySelector('.closeNavbar').textContent = "";
+}
+
+function openNav() {
+    if (document.getElementById("mySidebar").style.width == "250px") {
+        document.getElementById("mySidebar").style.width = "0"
+        document.querySelector('.closeNavbar').textContent = "";
+    } else {
+        document.getElementById("mySidebar").style.width = "250px";
+        document.querySelector('.closeNavbar').textContent = "x";
+    }
+
+    // document.getElementById("mySidebar").style.width = document.getElementById("mySidebar").style.width == "250px" ? "0" : "250px";
+}
+
+function SubmitForm() {
+    if (confirm('Наистина ли излезе от акаунта си?') === true) {
+        $('#submitForm').submit();
+    }
+}
